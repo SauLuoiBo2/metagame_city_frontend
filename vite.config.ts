4c7f0e9c4799f44f2070acc29e76f92d182a66e2
Shine.Dev <shine.edu.dev@gmail.com>
2022-08-21 14:18:21 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
     resolve: {
         alias: [
             { find: "@", replacement: path.resolve(__dirname, "./src") },
