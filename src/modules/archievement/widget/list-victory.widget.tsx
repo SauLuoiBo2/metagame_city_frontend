68e5e3cc099ff05b18de3559866b221e5eedfb98
Shine.Dev <shine.edu.dev@gmail.com>
2022-08-23 22:17:58 +0700

    hi

--- src/modules/archievement/widget/list-victory.widget.tsx
+++ src/modules/archievement/widget/list-victory.widget.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "@mui/material";
 import React from "react";
 
+import { useQueryGames } from "@/api";
 import { IMAGE_URL } from "@/assets/images";
 import { FrameTableCom } from "@/components";
 
 export interface ListVictoryWidgetProps {}
 
 const ListVictoryWidget: React.FC<ListVictoryWidgetProps> = () => {
+    const { useGetListVictory } = useQueryGames();
+    const { data } = useGetListVictory();
     return (
         <>
             <FrameTableCom imgTitle={IMAGE_URL.TITLE.TITLE_VICTORY} maxWidth={"80rem"}>
