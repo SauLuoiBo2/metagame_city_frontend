566bf349ec93ab295c095cf1cfd65ac2e4f75668
khoihien <khoihien@gmail.com>
2022-09-05 11:13:46 +0700

    loi chinh ta

--- src/libs/formik/index.ts
+++ src/libs/formik/index.ts
@@ -8,29 +8,29 @@ export function supportErrorFormik(formik: any, name: any): string | undefined {
 const regCode = /[0-9]{6}$/;
 
 export const yupChema = {
-    username: Yup.string().required("username is required").max(50, "username is max 100 word").nullable(),
+    username: Yup.string().required("Username is required").max(50, "Username is max 100 word").nullable(),
     email: Yup.string()
-        .email("email must match type email")
-        .required("email is required")
-        .max(100, "emaill is max 100 word")
+        .email("Email must match type email")
+        .required("Email is required")
+        .max(100, "Email is max 100 word")
         .nullable(),
     passwordCurrent: Yup.string()
-        .required("current password is required")
-        .max(32, " current password is max 32 word")
+        .required("Current password is required")
+        .max(32, " Current password is max 32 word")
         .nullable(),
-    password: Yup.string().required("password is required").max(32, "password is max 32 word").nullable(),
+    password: Yup.string().required("Password is required").max(32, "Password is max 32 word").nullable(),
 
-    newPassword: Yup.string().required("password is required").max(32, "password is max 32 word").nullable(),
+    newPassword: Yup.string().required("Password is required").max(32, "Password is max 32 word").nullable(),
     passwordConfirmNew: Yup.string()
-        .required("confirm password is required")
-        .oneOf([Yup.ref("passwordNew"), null], "confirm password not match new password"),
+        .required("Confirm password is required")
+        .oneOf([Yup.ref("passwordNew"), null], "Confirm password not match new password"),
     confirmPassword: Yup.string()
-        .required("confirm password is required")
-        .oneOf([Yup.ref("newPassword"), null], "confirm password not match password"),
+        .required("Confirm password is required")
+        .oneOf([Yup.ref("newPassword"), null], "Confirm password not match password"),
     passwordConfirm: Yup.string()
-        .required("confirm password is required")
-        .oneOf([Yup.ref("password"), null], "confirm password not match password"),
+        .required("Confirm password is required")
+        .oneOf([Yup.ref("password"), null], "Confirm password not match password"),
     codeVerifyGoogle: Yup.string()
-        .required("code is required")
-        .matches(regCode, "a verify google code is only number character anh exactly 6 character"),
+        .required("Code is required")
+        .matches(regCode, "A verify google code is only number character anh exactly 6 character"),
 };
